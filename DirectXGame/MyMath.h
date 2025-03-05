b882314487d7835594f5acb589608b7cf74a5aac
oosakiharuki <k023g0035@g.neec.ac.jp>
2025-03-05 10:20:04 +0900

    player動かした

---
+++ DirectXGame/MyMath.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "math/Matrix4x4.h"
+#include "math/Vector3.h"
+#include <cmath>
+
+//struct AABB {
+//	Vector3 min;
+//	Vector3 max;
+//};
+
+using namespace KamataEngine;
+
+namespace MyMath {
+
+	Vector3 operator+(const Vector3& v1, const Vector3& v2);
+	Vector3 operator-(const Vector3& v1, const Vector3& v2);
+	Vector3 operator*(const Vector3& v1, const Vector3& v2);
+	Vector3 operator/(const Vector3& v1, const Vector3& v2);
+
+	Vector3 operator*(const Vector3& v, const float f);
+	Vector3 operator*(const float f, const Vector3& v);
+
+	Vector3& operator+=(Vector3& v1, const Vector3& v2);
+	Vector3& operator-=(Vector3& v1, const Vector3& v2);
+	Vector3& operator*=(Vector3& v1, const Vector3& v2);
+	Vector3& operator/=(Vector3& v1, const Vector3& v2);
+
+	Matrix4x4 MakeIdentity4x4();
+	Matrix4x4 MakeScaleMatrix(Vector3 scale);
+	Matrix4x4 MakeRotateXMatrix(float radian);
+	Matrix4x4 MakeRotateYMatrix(float radian);
+	Matrix4x4 MakeRotateZMatrix(float radian);
+	Matrix4x4 MakeTranslateMatrix(Vector3 translate);
+
+	Vector3 Normalize(const Vector3& v);
+
+	#pragma region Affine
+
+	Matrix4x4 Multiply(Matrix4x4 m1, Matrix4x4 m2);
+
+	Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+	#pragma endregion
+
+	#pragma region 逆数
+	Matrix4x4 Inverse(const Matrix4x4& m);
+	#pragma endregion
+
+	Matrix4x4 MakePerspectiveFovMatrix(float forY, float aspectRatio, float nearClip, float farClip);
+
+	Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+
+} // namespace MyMath
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -3,6 +3,7 @@
 #include "math/Vector3.h"
 #include "3d/Camera.h"
 #include "3d/Model.h"
+#include "input/input.h"
 
 using namespace KamataEngine;
 
@@ -13,9 +14,14 @@ public:
 	void Draw();
 
 private:
-	WorldTransform worldTranform;
+	WorldTransform worldTransform;
 	Camera* viewProjection_ = nullptr;
 	Model* model_ = nullptr;
 
 	Vector3 position;
+	Vector3 velocity;
+	bool IsJump = false;
+
+	XINPUT_STATE state, preState;
+	const float speed = 0.2f;
 };
