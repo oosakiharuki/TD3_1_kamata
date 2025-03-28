4f98b47ecc8cc1053e1595cbb9ad9452b954fbb4
ryuto-alt <146914455+ryuto-alt@users.noreply.github.com>
2025-03-28 15:39:57 +0900

    マップ変更

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -34,7 +34,7 @@ void GameScene::Initialize() {
 	// 天球の生成
 	skydome_ = new Skydome();
 	// 天球3Dモデルの生成
-	modelSkydome_ = Model::CreateFromOBJ("space", true);
+	modelSkydome_ = Model::CreateFromOBJ("sky", true);
 	// 天球の初期化
 	skydome_->Initialize(modelSkydome_, &camera_);
 
